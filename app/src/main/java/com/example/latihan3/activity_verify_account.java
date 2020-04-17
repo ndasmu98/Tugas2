ed0fca439cac822b46310d06dbb530f36f54daf2
ndasmu98 <herdiansyahian15@gmail.com>
2020-04-17 23:29:25 +0700

    First commit

---
+++ app/src/main/java/com/example/latihan3/activity_verify_account.java
@@ -0,0 +1,14 @@
+package com.example.latihan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_verify_account extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_account);
+    }
+}
